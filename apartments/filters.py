4b6abaed24937a86009aa6905e54b9cc30b384bd
Atticus Ezis <atticus.ezis@gmail.com>
2026-01-14 15:35:04 +0900

    added filtering by status for managed Apartments

--- apartments/filters.py
+++ apartments/filters.py
@@ -2,16 +2,23 @@ from django_filters import rest_framework as filters
 from apartments.models import Apartment
 
 
-class ApartmentFilter(filters.FilterSet):
+class PublicApartmentFilter(filters.FilterSet):
     min_rent = filters.NumberFilter(field_name="monthly_rent", lookup_expr="gte")
     max_rent = filters.NumberFilter(field_name="monthly_rent", lookup_expr="lte")
     min_bedrooms = filters.NumberFilter(field_name="bedrooms", lookup_expr="gte")
     min_bathrooms = filters.NumberFilter(field_name="bathrooms", lookup_expr="gte")
     min_area = filters.NumberFilter(field_name="area", lookup_expr="gte")
+
+    class Meta:
+        model = Apartment
+        fields = ["monthly_rent", "bedrooms", "bathrooms", "area"]
+
+
+class PrivateApartmentFilter(filters.FilterSet):
     status = filters.ChoiceFilter(
         field_name="status", choices=Apartment.StatusChoices.choices
     )
 
     class Meta:
         model = Apartment
-        fields = ["monthly_rent", "bedrooms", "bathrooms", "area", "status"]
+        fields = ["status"]
--- apartments/views.py
+++ apartments/views.py
@@ -3,33 +3,37 @@ from apartments.models import Apartment
 from apartments.permissions import ApartmentOwnership
 from apartments.serializers import ApartmentSerializer
 from rest_framework.permissions import AllowAny, IsAuthenticated, DjangoModelPermissions
-from apartments.filters import ApartmentFilter
+from apartments.filters import PrivateApartmentFilter, PublicApartmentFilter
 from rest_framework import viewsets
+
+address_fields = [
+    "street_line_1",
+    "street_line_2",
+    "unit_number",
+    "city",
+    "state",
+    "postal_code",
+    "country",
+]
 
 
 # Public record, no auth required
 # filters by rent, bedrooms, bathrooms, area, status
 class ApartmentPublicListView(generics.ListAPIView):
     permission_classes = [AllowAny]
-    filterset_class = ApartmentFilter
-    queryset = Apartment.objects.filter(status=Apartment.StatusChoices.VACANT)
     serializer_class = ApartmentSerializer
+    # filter
+    filterset_class = PublicApartmentFilter
+    queryset = Apartment.objects.filter(status=Apartment.StatusChoices.VACANT)
+    search_fields = [address_fields]
 
 
 # Add a custom create to assign owner
 class ApartmentViewSet(viewsets.ModelViewSet):
     permission_classes = [DjangoModelPermissions, IsAuthenticated, ApartmentOwnership]
     serializer_class = ApartmentSerializer
-    search_fields = [
-        "street_line_1",
-        "street_line_2",
-        "unit_number",
-        "city",
-        "state",
-        "postal_code",
-        "country",
-    ]
-    # filter field for status
+    filterset_class = PrivateApartmentFilter
+    search_fields = [address_fields]
     ordering_fields = ["created_at"]
     ordering = ["-created_at"]
 
